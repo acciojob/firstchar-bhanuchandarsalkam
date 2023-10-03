b0d989f338d78486e51bb65cb999995e1f3430f3
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-10-03 07:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstChar(text) {
   // your code here
+	if(text==' '){
+		console.log(" ");
+	}
+	else{
+		for(let i=0;i<text.length;i++){
+			if(text[i]==' '){
+				continue;
+			}
+			else{
+				return text[i];
+				break;
+			}
+		}
+	}
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
+
+
+
 alert(firstChar(text));
